9596fae0773907ecd47c88eccb90d1414c6d34b5
Mugisha Yves <mugishayves189000@gmail.com>
2024-01-04 19:30:08 +0200

    made the project model

--- src/api/models/Project.ts
+++ src/api/models/Project.ts
@@ -1,38 +1,25 @@
 import { Schema, model, Model, Document, Types } from "mongoose";
 
-export interface IUser {
+export interface IProject {
   name: string;
   type: string;
-  customer:Types.ObjectId;
-  profile: string;
-  about?: string;
-  workExperience: Types.ObjectId[];
-  education: Types.ObjectId[];
-  skills: Types.ObjectId[];
-  languages: Types.ObjectId[];
-  awards: Types.ObjectId[];
-  files: Types.ObjectId[];
-  dob: Date;
-  gender: "MALE" | "FEMALE" | "NONE";
-  location: string;
+  customer: Types.ObjectId;
+  documents: string[];
+  tasks: Types.ObjectId[];
+  description: string;
+  budget: number;
+  timeline: number;
 }
 
-type UserModel = Model<IUser, Record<string, never>>;
-
-const schema = new Schema<IUser, UserModel>(
+const schema = new Schema<IProject>(
   {
-    username: { type: String, required: true },
-    about: { type: String, required: false },
-    profile: { type: String, required: false },
-    location: { type: String },
-    dob: { type: Date },
-    workExperience: [{ type: Schema.ObjectId, ref: "Work" }],
-    awards: [{ type: Schema.ObjectId, ref: "Award" }],
-    languages: [{ type: Schema.ObjectId, ref: "Language" }],
-    education: [{ type: Schema.ObjectId, ref: "Education" }],
-    files: [{ type: Schema.ObjectId, ref: "Work" }],
-    skills: [{ type: Schema.ObjectId, ref: "Skills" }],
-    gender: { type: String, default: "NONE" },
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    documents: [{ type: String, required: true }],
+    tasks: [{ type: Schema.ObjectId, required: true }],
+    budget: { type: Number, required: false },
+    timeline: { type: Number, required: false },
+    customer: { type: Schema.ObjectId, ref: "Customer" },
   },
   {
     timestamps: true,
@@ -40,7 +27,7 @@ const schema = new Schema<IUser, UserModel>(
 );
 
 schema.index({
-  username: "text",
+  name: "text",
 });
 
 schema.set("toJSON", {
@@ -51,10 +38,6 @@ schema.set("toJSON", {
   },
 });
 
-schema.virtual("role").get(function () {
-  return "USER";
-});
+const Project = model<IProject>("Project", schema);
 
-const User = model<IUser, UserModel>("User", schema);
-
-export default User;
+export default Project;
